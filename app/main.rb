8831a1308c75d914c078ee8661b029efbd9269a7
Sahil Jain <sahilbot1@gmail.com>
2019-12-09 14:14:03 -0600

    Added class implementation. Tried hashing but didn't fully work

--- app/main.rb
+++ app/main.rb
@@ -1,16 +1,52 @@
+class Box
+    attr_reader :id, :location, :threshold_val, :visible, :money_rate, :current_time, :current_level,
+                :final_level, :price, :vis_threshold
+
+    def initialize (id_num, x, y)
+        @id            = id_num
+        @location      = [x, y]
+        @vis_threshold = 10**id_num
+        @visible       = false
+        @money_rate    = @id**5
+        @current_time  = 0
+        @current_level = 0
+        @price         = 2 * 10**id_num
+        @final_level   = 5
+        @time_interval = 10
+        puts("Testing?" + " " + vis_threshold.to_s)
+    end
+
+    def update (current_money)
+        @current_time += 1 if current_level != 0
+        update_visibility(current_money)
+        return update_money(current_money)
+    end
+
+    def update_visibility (current_money)
+        @visible = true if current_money >= @vis_threshold && @visible == false
+    end
+
+    def update_money (current_money)
+        current_money += @money_rate if @current_time == @time_interval
+        return current_money
+    end
+
+    def purchase (current_money)
+        if @price < current_money && @current_level != @final_level
+            current_money -= @price
+            @price *= 1.25
+            @current_level += 1
+            @time_interval /= 2
+            @money_rate *= 1.25
+        end
+        return current_money
+    end
+    
+end
+
 class Clicker
     attr_accessor :grid, :inputs, :state, :outputs
 
-    def box
-        #location: {
-        #    0 => [1, 1],
-        #}
-        #visible: false
-        #money_rate
-        #time_rate
-        #current_time
-    end
-    
     def tick
         defaults
         render
@@ -19,38 +55,48 @@ class Clicker
     end
 
     def defaults
-        state.main_size ||= 300
+        #instantiate all of the defaults
+        state.main_size     ||= 300
         state.constant_size ||= state.main_size
-        state.money     ||= 0
-        state.moneyPosX ||= 635
-        state.rates     ||= [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        state.visible   ||= [true, false, false, false, false, false, false, false, false, false]
-        #state.visible   ||= [true, true, true, true, true, true, true, true, true, true] #for GUI
-        state.boxesCord ||= [750, 600]
-        state.rectSize  ||= [150, 50]
-        state.distance  ||= [state.rectSize[0] + 25, state.rectSize[1] + 25]
+        state.money         ||= 0
+        state.boxes_cord    ||= [750, 600]
+        state.rect_size     ||= [150, 50]
+        state.distance      ||= [state.rect_size[0] + 25, state.rect_size[1] + 25]
+        state.num_boxes     ||= 5
+        state.boxes         ||= []
+
+        
+        #create all of the boxes and instantiate their attributes
+        if state.boxes == []
+            deltaX = 0
+            deltaY = 0
+            state.num_boxes.times do |count|
+                state.boxes.push(Box.new(count + 1, state.boxes_cord[0] + (state.distance[0] * deltaX),
+                                         state.boxes_cord[1] - (state.distance[1] * deltaY)))
+                deltaX += 1
+                if deltaX % 3 == 0
+                    deltaX = 0
+                    deltaY += 1
+                end
+            end
+        end
     end
 
     def render
+        #get the basic button in the middle (the clicker)
         outputs.sprites << [500 - (state.main_size / 2), 360 - (state.main_size / 2),
                             state.main_size, state.main_size, "sprites/circle-gray.png"]
 
+        #get the current amount of money present
         outputs.labels << [145, 680, "Money:"]
-        outputs.labels << [163, state.moneyPosX, "$" + state.money.to_s]
+        outputs.labels << [170, 635, "$" + state.money.to_s, 1, 1]
         outputs.borders << [75, 600, 200, 50]
 
-        deltaX = 0
-        deltaY = 0
-        state.visible.length.times do |n|
-            if state.visible[n] == true
-                outputs.borders << [state.boxesCord[0] + (state.distance[0] * deltaX),
-                                    state.boxesCord[1] - (state.distance[1] * deltaY),
-                                    state.rectSize[0], state.rectSize[1]]
-            end
-            deltaX += 1
-            if deltaX % 3 == 0
-                deltaX = 0
-                deltaY += 1
+        #render boxes if they're visible
+        state.num_boxes.times do |n|
+            if state.boxes[n].visible == true
+                    outputs.borders << [state.boxes[n].location[0], state.boxes[n].location[1],
+                                        state.rect_size[0], state.rect_size[1]]
             end
         end
     end
@@ -59,16 +105,32 @@ class Clicker
         if state.money < 0
             puts("MISTAKE SOMEWHERE")
         end
+
+        #update animation of clicking the button
         if state.main_size > state.constant_size
             state.main_size -= 1
         end
+
+        #update the boxes based on time intervals and money
+        state.num_boxes.times do |n|
+            state.money = state.boxes[n].update(state.money)
+        end
+        
     end
 
     def process_inputs
+        #update basic money increases from pressing space
         if inputs.keyboard.key_down.space && state.constant_size == state.main_size
-            state.money += state.rates[0]
+            state.money += 1
             state.main_size += 7
             inputs.keyboard.clear
+        end
+
+        #add purchasing
+        state.num_boxes.times do |n|
+            if inputs.keyboard.key_down.((":" + n.to_s).to_e)
+                state.money = state.boxes[n - 1].purchase(state.money)
+            end
         end
     end
 
